b6b36f670aae594a50ad9ce22066f21755c591c5
Aemaeth <aemaeth@RoyAylas-MacBook-Pro.local>
2020-03-17 21:21:45 +0800

    use git deps

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule EmqxPluginDemo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:emqx, path: "../emqx" }
+      {:emqx, git: "https://github.com/emqx/emqx.git", branch: "master" }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
