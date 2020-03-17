474d271ea6a9da483ee07f33e0ba7ba23433c532
Aemaeth <aemaeth@RoyAylas-MacBook-Pro.local>
2020-03-17 17:47:54 +0800

    added console log on connected

--- lib/emqx_plugin_demo/emqx_plugin_demo_body.ex
+++ lib/emqx_plugin_demo/emqx_plugin_demo_body.ex
@@ -1,9 +1,16 @@
 defmodule EmqxPluginDemo.Body do
 
-  def load(_env) do
+  def load(env) do
+    :emqx.hook(:"client.connect", &EmqxPluginDemo.Body.on_client_connected/3, [env])
 
   end
 
+  def on_client_connected(returncode, client, env) do
+    IO.inspect(["elixir on_client_connected", client, returncode, env])
+    :ok
+  end
+
   def unload do
+    :emqx.unhook(:"client.connect", &EmqxPluginDemo.Body.on_client_connected/3)
   end
 end
